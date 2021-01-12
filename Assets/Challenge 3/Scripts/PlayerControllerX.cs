0de0494187d8a923990039b165fe3e40ed3251e3
Danylov <ol_danylov@ukr.net>
2021-01-12 11:54:18 +0200

    First commit

---
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver = false;
+
+    public float floatForce = 50.0f;
+    private float gravityModifier = 0.01f; //1.5f;
+    private Rigidbody playerRb;
+    private bool spacePressed = false;
+    
+    private float highLevel = 14.0f;
+    
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip groundSound;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(0.0f * Vector3.up, ForceMode.Impulse);
+        playerRb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) && spacePressed) && !gameOver)
+        {
+            if (Input.GetKey(KeyCode.Space))  spacePressed = true;
+            playerRb.useGravity = true;
+            playerRb.AddForce(floatForce * Vector3.up, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && spacePressed && !gameOver)
+        {
+            playerRb.useGravity = true;
+            playerRb.AddForce(floatForce * Vector3.down, ForceMode.Impulse);
+        }
+        if (highLevel < transform.position.y)
+        {
+            playerRb.useGravity = false;
+            playerRb.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        }
+        // if player collides with ground
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+         if (spacePressed){
+            playerAudio.PlayOneShot(groundSound, 1.0f);
+            playerRb.velocity = Vector3.zero;
+            playerRb.AddForce(3.0f * floatForce * Vector3.up, ForceMode.Impulse);
+         }
+        }
+    }
+
+}
---
+++ Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] objectPrefabs;
+    private const float SpawnDelay = 2;
+    private const float SpawnInterval = 1.5f;
+
+    private PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnObject), SpawnDelay, SpawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Spawn obstacles
+    private void SpawnObject()
+    {
+        // Set random spawn location and random object index
+        var spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        var index = Random.Range(0, objectPrefabs.Length);
+
+        // If game is still active, spawn new object
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
+
+    }
+}
